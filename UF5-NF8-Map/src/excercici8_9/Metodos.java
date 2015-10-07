bc836b514b95478a907408ced070afe7153d3d9f
isma <ismael.felipa@gmail.com>
2015-10-07 15:03:12 +0200

    +avances

---
+++ UF5-NF8-Map/src/excercici8_9/Metodos.java
@@ -0,0 +1,20 @@
+package excercici8_9;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import exercici8_8.Cotxe;
+
+public class Metodos {
+	public static void removeCar(Cotxe element) {
+
+	}
+
+	public static ArrayList<Cotxe> nameCar(int nCili, List<Cotxe> list) {
+
+	}
+	
+	public static ArrayList<Cotxe> barndCar(String  nCili, List<Cotxe> list) {
+
+	}
+}
